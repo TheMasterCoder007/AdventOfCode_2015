5e3f52e957f564201319620228db47e3a57c6369
Cory Lamming <cory@spydercontrols.com>
2022-06-12 09:42:56 -0700

    Refactored code to use header file and seperate .c files for each days challenge

---
+++ AdventOfCode.h
@@ -0,0 +1,12 @@
+//
+// Created by themastercoder007 on 2022-06-12.
+//
+
+#ifndef ADVENTOFCODE_H
+#define ADVENTOFCODE_H
+
+// declare functions
+int main();
+void aocDay1();
+
+#endif //ADVENTOFCODE_H
--- main.c
+++ main.c
@@ -1,10 +1,12 @@
+//
+// Created by themastercoder007 on 2022-06-10.
+//
+
+#include "AdventOfCode.h"
 #include <stdio.h>
 
 int main()
 {
-    // declare functions
-    void aocDay1();
-
     // variables
     static int command;
     static char ch;
@@ -45,66 +47,4 @@ int main()
     }
 
     return 0;
-}
-
-/********************************************
-*                DAY 1                      *
-********************************************/
-
-void aocDay1()
-{
-    // declare functions
-    int main();
-
-    // variables
-    static int floor = 0;
-    static int chPos = 0;
-    static int position = 0;
-    static char ch;
-    static int enter;
-
-    // open text file
-    FILE *input = fopen("Input_Files/Input_1.txt", "r");
-
-    // check for problem reading file
-    if (input == NULL)
-    {
-        printf("There was a problem opening the file Input_1.txt\n\n");
-    }
-    else
-    {
-        // run code to calculate floor
-        while ((ch = fgetc(input)) != EOF)
-        {
-            //increment character position
-            chPos++;
-
-            if (ch == '(')
-            {
-                floor++;
-            }
-            else if (ch == ')')
-            {
-                floor--;
-            }
-
-            // update position if it is the first time santa entered the basement
-            if (position == 0 && floor == -1)
-            {
-                position = chPos;
-            }
-        }
-
-        printf("The floor Santa needs to be on is %d\n", floor);
-        printf("The position of the character when santa first entered the basement is %d\n\n", position);
-    }
-    // close text file
-    fclose(input);
-
-    printf("Please press enter to continue...");
-    fflush(stdin);
-    scanf("&d", &enter);
-
-    // re-enter main function
-    main();
 }
